ba4d855022dc4e9e436099e9926123e7d6b53a69
Ashwin Ramachandran <ashwinram1@gmail.com>
2017-08-21 13:10:24 -0700

    colored rectangle moves across now

--- js/app.js
+++ js/app.js
@@ -21,22 +21,24 @@ var canvasCtx, coordX, coordY, stopIntId;
 
 var outOfBounds = function() {
 	var outOfBounds = (coordX > canvas.attr('width')) || (coordY > canvas.attr('height'));
+	console.log(outOfBounds);
 	return outOfBounds;
 };
 
 var stopIntervalWork = function() {
+	console.log('stopped the interval');
 	clearInterval(stopIntId);
 };
 
 coordX = 0;
 coordY = 0;
 
-canvasCtx.font = "15px serif";
+canvasCtx.fillStyle = "rgb(129, 159, 165)";
 
 stopIntId = setInterval(function() {
 
 	canvasCtx.clearRect(0, 0, canvas.attr('width'), canvas.attr('height'));
-	canvasCtx.fillText("Hello World!", coordX, coordY);
+	canvasCtx.fillRect(coordX, coordY, 10, 10);
 	coordX++;
 	coordY++;
 
@@ -45,3 +47,6 @@ stopIntId = setInterval(function() {
 	}
 
 }, 1000/30);
+
+//rgb(95, 110, 132)
+//rgb(129, 159, 165)
